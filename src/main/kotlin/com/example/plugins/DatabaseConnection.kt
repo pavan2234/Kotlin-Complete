a048828e6600dbad304fb28f1d3c8ef542acea8b
Ananthoju Pavan <101407548+pavan2234@users.noreply.github.com>
2022-10-12 14:56:25 +0530

    Kotlin

---
+++ src/main/kotlin/com/example/plugins/DatabaseConnection.kt
@@ -0,0 +1,11 @@
+package com.example.plugins
+
+import me.liuwj.ktorm.database.Database
+
+object DatabaseConnection{
+    val url="jdbc:mysql://localhost:3306/student"
+    val driver="com.mysql.cj.jdbc.Driver"
+    val user="root"
+    val password="root"
+    val database= Database.connect(url,driver, user, password)
+}
---
+++ src/main/kotlin/com/example/plugins/UserDetails.kt
@@ -0,0 +1,25 @@
+package com.example.plugins
+
+import io.ktor.server.application.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import me.liuwj.ktorm.dsl.insert
+
+fun Application.UserDetails(){
+    val db=DatabaseConnection.database
+    routing {
+        get ("/registration"){
+            val userDetails=call.receive<StudentCredentials>()
+
+            val name=userDetails.name
+            val password=userDetails.password
+
+            db.insert(student){
+                set(it.name,name)
+                set(it.password,password)
+            }
+            call.respondText("Succesfully inserted")
+        }
+    }
+}
